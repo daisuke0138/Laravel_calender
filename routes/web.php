b8d3a98483f0e69726c38b1437335adb531d5c57
daisuke0138 <daisuke_arsp@hotmail.com>
2024-10-31 23:44:44 +0900

    カレンダー初回push

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScheduleController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome');});
+Route::get('/calendar', function () {return view('calendar');});
+
+// イベント登録処理
+Route::post('/schedule-add', [ScheduleController::class, 'scheduleAdd'])->name('schedule-add');
+
+// イベント取得処理
+Route::post('/schedule-get', [ScheduleController::class, 'scheduleGet'])->name('schedule-get');
