7a253b0d767c7afe218985dd41f66f32052dee29
daisuke0138 <daisuke_arsp@hotmail.com>
2024-11-04 00:44:00 +0900

    build

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -32,7 +32,7 @@ const calendar = new Calendar(calendarEl, {
                 // .post("/schedule-add", {
 
                 // 本番環境のエンドポイント
-                .post("/schedule-add", {
+                .post("my-calender/calendar/schedule-add", {
                     start_date: info.start.valueOf(),
                     end_date: info.end.valueOf(),
                     event_name: eventName,
@@ -61,7 +61,7 @@ const calendar = new Calendar(calendarEl, {
             // .post("/schedule-get", {
 
             // 本番環境のエンドポイント
-            .post("/schedule-get", {
+            .post("/my-calender/calendar/schedule-get", {
                 start_date: info.start.valueOf(),
                 end_date: info.end.valueOf(),
             })
